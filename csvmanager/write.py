a9b8c383ef9fcf8219b4d27ba0481b91c5d0949c
tjd5 <80058562+tjd5@users.noreply.github.com>
2021-12-15 16:23:46 -0500

    Add files via upload

---
+++ csvmanager/write.py
@@ -0,0 +1,7 @@
+import os
+
+
+class Write:
+    @staticmethod
+    def DataFrameToCSVFile(filename, df):
+        return df.to_csv(os.path.abspath(filename), float_format='%.2f', index=True, header=True)
