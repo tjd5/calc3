c6f53d80b0a71a66290a661e0baa603c1947fce7
tjd5 <80058562+tjd5@users.noreply.github.com>
2021-12-15 16:04:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-"""Content of calculator.py#"""
+""" content of calculator.py#"""
+
+
 def inc(x_value):
-    """Increment function adds one to the x_value"""
+    """ Increment Function adds one to the x_value"""
+
     return x_value + 1
+
+
 def test_answer():
-    """This tests the function"""
+    """This Tests the function"""
     assert inc(4) == 5
