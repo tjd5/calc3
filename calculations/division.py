68fbf6eefd26f13dbf9a59ed152a35af362c3cba
tjd5 <80058562+tjd5@users.noreply.github.com>
2021-12-15 15:54:58 -0500

    Delete division.py

--- calculations/division.py
+++
@@ -1,8 +0,0 @@
-from calc.calculations.calculation import Calculation
-
-class Divison(Calculation):
-    def getresult(self):
-        result = 1.0
-        for value in self.values:
-            result = result // value
-        return result
