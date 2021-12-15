db231147228d52c1cd7aeaf37eab26dc0b6ad7cd
tjd5 <80058562+tjd5@users.noreply.github.com>
2021-12-15 15:59:26 -0500

    Delete subtraction_test.py

--- tests/subtraction_test.py
+++
@@ -1,6 +0,0 @@
-from calc.calculations.subtraction import Subtraction
-
-def test_calculation_subtraction():
-    mynumbers = (1.0,2.0)
-    subtraction = Subtraction(mynumbers)
-    assert subtraction.get_result() == -3
